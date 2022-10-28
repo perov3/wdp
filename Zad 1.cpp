a234d125953efcd85d464316058b5d2948452728
perov3 <115549645+perov3@users.noreply.github.com>
2022-10-28 10:24:09 +0200

    Add files via upload

---
+++ Zad 1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "Wpisz cene produktu:" << endl;
+	cin >> a;
+	cout << "Wpisz cene sprzedazy:" << endl;
+	cin >> b;
+	int c = b - a;
+	if (c > 0) {
+		cout << "Zysk:" << c;
+	}
+	if (c < 0) {
+		cout << "Strata:" << -c;
+	}
+	if (c == 0) {
+		cout << "Brak zysku i straty.";
+	}
+}
