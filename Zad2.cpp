0e612811db73d38bd44ec1d951ba070b9631f907
perov3 <115549645+perov3@users.noreply.github.com>
2022-10-28 10:56:06 +0200

    Add files via upload

---
+++ Zad2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Wpisz rok:" << endl;
+    cin >> a;
+    if (a % 4 == 0, a % 100 != 0, a % 400 == 0) {
+        cout << "Rok jest przestepny.";
+    }
+    else if (a % 4 == 0, a % 100 != 0, a % 400 != 0) {
+        cout << "Rok nie jest przestepny.";
+    }
+    else if (a % 4 != 0) {
+        cout << "Rok nie jest przestepny.";
+    }
+}
